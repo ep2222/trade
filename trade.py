ab7ec65f5afa354a33e8780cda52cd7f51e56b2e
Evan Price <evanprice@ou.edu>
2024-08-08 14:59:49 -0500

    prices

--- trade.py
+++ trade.py
@@ -1,8 +1,15 @@
+import collections
+from collections import abc
+collections.MutableMapping = abc.MutableMapping
+collections.Mapping = abc.Mapping
+collections.Sequence = abc.Sequence
+collections.Callable = abc.Callable
 import os
 import time
 from datetime import datetime
 from binance.client import Client as bnc
 import ccxt
+import cbpro
 
 
 # write output to file
@@ -11,7 +18,7 @@ def output(message):
         wire.write(message)
 
 
-# find common assets among binance and coinbase api keys
+# find common assets between binance and coinbase api keys
 def syncWallets():
     # binance
     try:
@@ -34,14 +41,42 @@ def syncWallets():
     # shared
     shared = bnc_assets.intersection(cbc_assets)
     output(f"{shared}\n{len(shared)} Shared\n\n")
+    
+    # invalid
+    invalid = {"REP", "CELO", "USDC", "DAI", "USDT"}
+    output(f"{invalid}\n{len(invalid)} Invalid\n\n")
+
+    # valid
+    valid = shared - invalid
+    output(f"{valid}\n{len(valid)} Valid\n\n")
+    return valid
+
+
+# get spot price
+def getPrice(asset): 
+    try:
+        ticker = cbp.get_product_ticker(product_id = asset + "-USD")
+        return float(ticker['price'])
+    except Exception as e:  
+        output(f"{asset} Error {e}\n")
+        return None
 
 
 # track runtime
 start_time = time.time()
 wire_path = "/home/dev/code/tmp/" + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) + ".txt"
+cbp = cbpro.PublicClient()
 
 # void main
-syncWallets()
+shared = syncWallets()
+
+# capture prices at time of trade
+prices = dict()
+for asset in shared:
+    price = getPrice(asset)
+    if price:
+        prices[asset] = price
+output(f"{prices}\n{len(prices)} Prices\n\n")
 
 # calculate runtime
 end_time = time.time()
